1d2cb1781cc44ec30ad3a8392edc60154f6f39dc
tobedoit <tobedoit@aol.com>
2023-03-13 16:48:06 +0900

    edit

--- pages/projects.js
+++ pages/projects.js
@@ -16,7 +16,7 @@ export default function Projects({ projects }) {
       <Layout>
         <div className='flex flex-col px-4 xs:px-10 items-center justify-center min-h-screen'>
           <h1 className='text-4xl sm:text-6xl font-bold mt-12'>
-            총 프로젝트 : <span className='pl-4 text-blue-500 dark:text-blue-500'>{projects.results?.length}</span>
+            총 프로젝트 : <span className='pl-4 text-blue-500 dark:text-blue-500'>{projects.results.length}</span>
           </h1>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 xs:w-full py-10 xs:m-6 m-2'>
             {projects.results.map((project) => (
