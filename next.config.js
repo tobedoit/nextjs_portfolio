cf7834cd82f0370ce25314ab69f3ed5a6bb503b5
tobedoit <tobedoit@aol.com>
2023-03-15 19:12:02 +0900

    edit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     domains: [
       'www.notion.so',
-      'images.unsplash.com'
+      'images.unsplash.com',
+      's3.us-west-2.amazonaws.com'
     ]
   }
 }
